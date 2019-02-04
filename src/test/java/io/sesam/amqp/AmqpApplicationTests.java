1d670f4cf103ed117295bb15868b4efb089cf162
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2019-02-04 10:21:58 +0100

    Fix borken test

--- src/test/java/io/sesam/amqp/AmqpApplicationTests.java
+++ src/test/java/io/sesam/amqp/AmqpApplicationTests.java
@@ -1,5 +1,6 @@
 package io.sesam.amqp;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AmqpApplicationTests {
+
+	@BeforeClass
+	public static void setUp() {
+        System.setProperty("url", "foo");
+	}
 
 	@Test
 	public void contextLoads() {
